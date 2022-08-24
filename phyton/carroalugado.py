4969efe7355a598bcbffdc66161c63f28e8a0ef3
Luana Trevizani <luanaftrevizani@gmail.com>
2022-08-24 10:42:28 -0300

    Pasta adicionada errada deletada

--- phyton/carroalugado.py
+++
@@ -1,8 +0,0 @@
-km = int(input('Quantidade de km percorridos:'))
-dias = int(input('Quantos dias o carro foi alugado:'))
-
-preco = 60 * dias + 0.15 * km
-
-print('km rodado: {}. Dias Alugados: {}.'.format(
-    km, dias))
-print('Valor total: R$', preco)
--- phyton/ola.py
+++
@@ -1,5 +0,0 @@
-print('Olá, mundo!')
-print('2 + 3')  # 2 + 3
-print(2 + 3)  # Executa o cálculo, 5
-print('2' + '3')  # 23
-print('O resultado da soma de 2 + 3 é:', 2 + 3)
